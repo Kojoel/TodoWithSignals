838f6e6b7eb1bc324e6931bfd5410d6697d761c3
Kojoel <nanaabruquah1s@gmail.com>
2025-01-03 10:05:52 +0000

    feat: create seachbar for searching todo list

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SearchbarComponent } from '../components/shared/searchbar/searchbar.component';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [],
+  imports: [SearchbarComponent],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss'
 })
